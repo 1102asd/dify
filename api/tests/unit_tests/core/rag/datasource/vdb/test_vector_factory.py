e26d478dc60c0b42ce98e6315a53afcc366d8485
Bowen Liang <liangbowen@gf.com.cn>
2024-04-23 00:08:08 +0800

    test_milvus_vector

--- api/tests/unit_tests/core/rag/datasource/vdb/test_vector_factory.py
+++ api/tests/unit_tests/core/rag/datasource/vdb/test_vector_factory.py
@@ -1,6 +1,7 @@
 import uuid
 from typing import Optional
 
+from core.rag.datasource.vdb.milvus.milvus_vector import MilvusConfig, MilvusVector
 from core.rag.datasource.vdb.qdrant.qdrant_vector import QdrantConfig, QdrantVector
 from core.rag.datasource.vdb.vector_base import BaseVector
 from core.rag.datasource.vdb.weaviate.weaviate_vector import WeaviateConfig, WeaviateVector
@@ -53,6 +54,37 @@ def test_qdrant_vector():
                 api_key='difyai123456',
                 root_path='/',
                 timeout='20'
+            )
+        )
+        document = Document(
+            page_content='test',
+            metadata={
+                "doc_id": dataset_id,
+                "doc_hash": dataset_id,
+                "document_id": dataset_id,
+                "dataset_id": dataset_id,
+            }
+        )
+        vector.create(texts=[document], embeddings=[[1.1, 2.2, 3.3]])
+        vector.delete()
+
+
+def test_milvus_vector():
+    vector_type = 'qdrant'
+
+    vector: Optional[BaseVector] = None
+    if vector_type == "qdrant":
+        dataset_id = str(uuid.uuid4())
+        collection_name = Dataset.gen_collection_name_by_id(dataset_id)
+        vector = MilvusVector(
+            collection_name=collection_name,
+            config=MilvusConfig(
+                host='localhost',
+                port='19530',
+                user='root',
+                password='Milvus',
+                secure='false',
+                database='default',
             )
         )
         document = Document(
